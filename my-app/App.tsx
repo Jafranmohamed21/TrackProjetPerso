50f75d0333a874bae56794ea9cefb32eba367c94
Jafranmohamed21 <jafranmohamed93@gmail.com>
2022-06-30 15:09:15 +0200

    Fetch Studio Ghibili Api Data

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 
-var title = 'Hi';
 
-fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
-.then(response => response.json())
-.then(data => { title = data[0]});
 
 
 const HelloWorldApp = () => {
+  const [title, setTitle] = useState('')
+
+fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
+.then(response => response.json())
+.then(data => {setTitle(data.title)});
+
   return (
     <View
       style={{
