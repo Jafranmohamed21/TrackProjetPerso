e6ab879491de41ddc3f957b69a4c5c94144d72eb
Jafranmohamed21 <jafranmohamed93@gmail.com>
2022-07-03 20:37:04 +0200

    display several films data

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import MovieData from './fetchMovieData';
 
-var title = '';
-var image = '';
+var title = new Array();
+var image = new Array();
 
 const styles = StyleSheet.create({
   image: {
@@ -13,24 +13,31 @@ const styles = StyleSheet.create({
 });
 
 const HelloWorldApp = () => {
+
   const [data, setData] = useState('')
   MovieData((result) => {setData(result)});
- 
-  var tab = Object.getOwnPropertyNames(data);
-  var value = Object.values(data);
 
-  for (let i in tab) {
-    if (tab.hasOwnProperty(i)) {
-        if(tab[i] == 'title'){
-          title = value[i];
-        }
-        if(tab[i] == 'image'){
-          image = value[i];
-        }
-    }
-    else {
+  var value = Object.values(data);
+  var j = 0;
+
+  for (let n in value) {
+    var val = Object.values(value[n]);
+    var tab = Object.getOwnPropertyNames(value[n]);
+    for (let i in tab) {
+      if (tab.hasOwnProperty(i)) {
+          if(tab[i] == 'title'){
+            title[j] = val[i];
+          }
+          if(tab[i] == 'image'){
+            image[j] = val[i];
+            j++;
+          }
+      }
+      else {
+      }
     }
   }
+  //console.log(value[1]);
 
   return (
     <View
@@ -41,10 +48,16 @@ const HelloWorldApp = () => {
       }}>
       <Image
        style={styles.image}
-       source={{uri: image}}
+       source={{uri: image[0]}}
        resizeMode= 'contain'
       />
-      <Text>{title}</Text>
+      <Text>{title[0]}</Text>
+      <Image
+       style={styles.image}
+       source={{uri: image[1]}}
+       resizeMode= 'contain'
+      />
+      <Text>{title[1]}</Text>
     </View>
   );
 };
--- my-app/fetchMovieData.tsx
+++ my-app/fetchMovieData.tsx
@@ -1,7 +1,7 @@
 
 const MovieData = (callback: (arg: any) => void) => {
     
-    fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
+    fetch('https://ghibliapi.herokuapp.com/films')
     .then(response => response.json())
     .then(data => {callback(data)});
 }
